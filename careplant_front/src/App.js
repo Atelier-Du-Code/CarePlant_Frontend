e4fd0878a7876c5d5690f39991a7d8d6935f2cf3
manon.avaullee@epitech.eu <manon.avaullee@epitech.eu>
2025-02-07 19:08:39 +0100

    test depoiement 5

--- careplant_front/src/App.js
+++ careplant_front/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Deployement opérationnel !!</p>
+      <p>Deployement opérationnel !!!!!</p>
     </div>
   );
 }
